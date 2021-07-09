3bc32d60e2c9e747edf8819cbe284e0279d05140
mr3dh00d <dereck.pavez@outlook.com>
2021-07-09 11:22:04 -0400

    Commit 2

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/commit-2', (req, res) => {
+  res.status(200).json({'name':'commit-2'});
+});
+
 module.exports = router;
